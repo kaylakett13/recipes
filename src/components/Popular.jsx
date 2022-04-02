eaed6ab13ae57efc4d134aeb88bcabc5fc641bf5
Kayla Ketterer <kaylaketterer@Kaylas-MacBook-Air.local>
2022-04-01 22:39:57 -0400

    tutorial marker 24:28

--- src/components/Popular.jsx
+++ src/components/Popular.jsx
@@ -1,6 +1,30 @@
+import { useEffect, useState } from "react";
+
 function Popular() {
+
+    const[popular, setPopular] = useState([]);
+    
+    useEffect(() => {
+        getPopular();
+    },[]);
+
+    const getPopular = async () => {
+        const api = await fetch(`https://api.spoonacular.com/recipes/random?apiKey=5181d7bcc07e42f9a98f740fde9cfc28&number=9`);
+        const data = await api.json();
+        setPopular(data.recipes);
+        console.log(data);
+    }
   return (
-    <div>Popular</div>
+    <div>
+        {popular.map((recipe) => {
+        return(
+            <div>
+                <p>{recipe.title}</p>
+                <img src={recipe.image} alt={recipe.title} />
+            </div>
+        );
+    })}
+    </div>
   )
 }
 
